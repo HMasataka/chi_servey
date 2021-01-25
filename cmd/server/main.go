75b4d266703713743550caad16c5341ea7c4aa19
Masataka Hisasue <masataka.hisasue@knocknote.co.jp>
2021-01-25 15:49:33 +0900

    Feat custom middleware

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log"
@@ -12,6 +13,13 @@ import (
 	"github.com/go-chi/chi/middleware"
 )
 
+func customMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := context.WithValue(r.Context(), "user", "123")
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
 func main() {
 	var port = flag.Int("port", 8080, "Port for test HTTP server")
 	flag.Parse()
@@ -22,6 +30,7 @@ func main() {
 	r.Use(middleware.RequestID)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
+	r.Use(customMiddleware)
 
 	h := s.HandlerFromMux(petStore, r)
 	s := &http.Server{
--- pets.go
+++ pets.go
@@ -1,6 +1,7 @@
 package survey
 
 import (
+	"fmt"
 	"net/http"
 )
 
@@ -12,13 +13,21 @@ type Pets struct {
 }
 
 func (p *Pets) FindPets(w http.ResponseWriter, r *http.Request, params FindPetsParams) {
+	user := r.Context().Value("user").(string)
+	w.Write([]byte(fmt.Sprintf("hi %s", user)))
 }
 
 func (p *Pets) AddPet(w http.ResponseWriter, r *http.Request) {
+	user := r.Context().Value("user").(string)
+	w.Write([]byte(fmt.Sprintf("hi %s", user)))
 }
 
 func (p *Pets) FindPetById(w http.ResponseWriter, r *http.Request, id int64) {
+	user := r.Context().Value("user").(string)
+	w.Write([]byte(fmt.Sprintf("hi %s", user)))
 }
 
 func (p *Pets) DeletePet(w http.ResponseWriter, r *http.Request, id int64) {
+	user := r.Context().Value("user").(string)
+	w.Write([]byte(fmt.Sprintf("hi %s", user)))
 }
